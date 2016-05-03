b8254b7999fecf4be31aafda3da24727c62a4166
Lex Myers <lex.myers@gmail.com>
2016-05-03 09:22:42 -0700

    now returns object with all validations

--- sandbox/lex/validate.js
+++ sandbox/lex/validate.js
@@ -1,16 +1,19 @@
 function validate(str) {
-  var special = '`-=,!;@#$%^&*()_+{}|:"<>?';
-  var numbers = '1234567890';
-  var lowerCase = 'qwertyuiopasdfghjklzxcvbnm';
-  var upperCase = 'QWERTYUIOPASDFGHJKLZXCVBNM'
-  var hasSpecial = false;
-  var hasNumbers = false;
-  var hasLower = false;
-  var hasUpper = false;
+  const special = '`-=,!;@#$%^&*()_+{}|:"<>?';
+  const numbers = '1234567890';
+  const lowerCase = 'qwertyuiopasdfghjklzxcvbnm';
+  const upperCase = 'QWERTYUIOPASDFGHJKLZXCVBNM';
+  var result = {};
+  result.hasSpecial = false;
+  result.hasNumbers = false;
+  result.hasLower = false;
+  result.hasUpper = false;
   for(var i = 0; i < str.length; i++) {
     if(special.indexOf(str[i]) > -1) hasSpecial = true;
     if(numbers.indexOf(str[i]) > -1) hasNumbers = true;
     if(lowerCase.indexOf(str[i]) > -1) hasLower = true;
     if(upperCase.indexOf(str[i]) > -1) hasUpper = true;
   }
+  result.passes = hasSpecial && hasNumbers && hasLower && hasUpper;
+  return result;
 }
