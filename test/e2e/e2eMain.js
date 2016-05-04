4032a9e4f1ab0dc721336d6475bb3a203beb63a4
Lex Myers <lex.myers@gmail.com>
2016-05-04 15:36:02 -0700

    buttons now clickable in test

--- test/e2e/e2eMain.js
+++ test/e2e/e2eMain.js
@@ -2,45 +2,48 @@ describe('the very first test', function() {
   var magicInput = element(by.model('test.magicInput'));
   var siteInput = element(by.model('test.siteInput'));
 
-  var dogBox = element(by.cssContainingText('.box', 'Dog'));
-  var catBox = element(by.cssContainingText('.box', 'Cat'));
-  var rabbitBox = element(by.cssContainingText('.box', 'Rabbit'));
-  var dragonBox = element(by.cssContainingText('.box', 'Dragon'));
-  var llamaBox = element(by.cssContainingText('.box', 'Llama'));
-  var unicornBox = element(by.cssContainingText('.box', 'Unicorn'));
-  var eagleBox = element(by.cssContainingText('.box', 'Eagle'));
-  var dolphinBox = element(by.cssContainingText('.box', 'Dolphin'));
-  var penguinBox = element(by.cssContainingText('.box', 'Penguin'));
+  var dogBox = element(by.cssContainingText('.button', 'Dog'));
+  // var dogBox = element(by.id('dog'));
+  var catBox = element(by.cssContainingText('.button', 'Cat'));
+  var rabbitBox = element(by.cssContainingText('.button', 'Rabbit'));
+  var dragonBox = element(by.cssContainingText('.button', 'Dragon'));
+  // var llamaBox = element(by.css('ng-click=\"animals(\'llama\')\"'));
+  // var llamaBox = element(by.buttonText('Llama'));
+  var llamaBox = element(by.cssContainingText('.button', 'Llama'));
+  var unicornBox = element(by.cssContainingText('.button', 'Unicorn'));
+  var eagleBox = element(by.cssContainingText('.button', 'Eagle'));
+  var dolphinBox = element(by.cssContainingText('.button', 'Dolphin'));
+  var penguinBox = element(by.cssContainingText('.button', 'Penguin'));
 
-  var eatsBox = element(by.cssContainingText('.box', 'eats'));
-  var breathesBox = element(by.cssContainingText('.box', 'breathes'));
-  var drinksBox = element(by.cssContainingText('.box', 'drinks'));
-  var dreamsBox = element(by.cssContainingText('.box', 'dreams'));
-  var fartsBox = element(by.cssContainingText('.box', 'farts'));
-  var smellsBox = element(by.cssContainingText('.box', 'smells'));
-  var snubsBox = element(by.cssContainingText('.box', 'snubs'));
-  var snortsBox = element(by.cssContainingText('.box', 'snorts'));
-  var summonsBox = element(by.cssContainingText('.box', 'summons'));
+  var eatsBox = element(by.cssContainingText('.button', 'eats'));
+  var breathesBox = element(by.cssContainingText('.button', 'breathes'));
+  var drinksBox = element(by.cssContainingText('.button', 'drinks'));
+  var dreamsBox = element(by.cssContainingText('.button', 'dreams'));
+  var fartsBox = element(by.cssContainingText('.button', 'farts'));
+  var smellsBox = element(by.cssContainingText('.button', 'smells'));
+  var snubsBox = element(by.cssContainingText('.button', 'snubs'));
+  var snortsBox = element(by.cssContainingText('.button', 'snorts'));
+  var summonsBox = element(by.cssContainingText('.button', 'summons'));
 
-  var redBox = element(by.cssContainingText('.box', 'Red'));
-  var blueBox = element(by.cssContainingText('.box', 'Blue'));
-  var greenBox = element(by.cssContainingText('.box', 'Green'));
-  var yellowBox = element(by.cssContainingText('.box', 'Yellow'));
-  var orangeBox = element(by.cssContainingText('.box', 'Orange'));
-  var purpleBox = element(by.cssContainingText('.box', 'Purple'));
-  var whiteBox = element(by.cssContainingText('.box', 'White'));
-  var blackBox = element(by.cssContainingText('.box', 'Black'));
-  var rainbowBox = element(by.cssContainingText('.box', 'Rainbow'));
+  var redBox = element(by.cssContainingText('.button', 'Red'));
+  var blueBox = element(by.cssContainingText('.button', 'Blue'));
+  var greenBox = element(by.cssContainingText('.button', 'Green'));
+  var yellowBox = element(by.cssContainingText('.button', 'Yellow'));
+  var orangeBox = element(by.cssContainingText('.button', 'Orange'));
+  var purpleBox = element(by.cssContainingText('.button', 'Purple'));
+  var whiteBox = element(by.cssContainingText('.button', 'White'));
+  var blackBox = element(by.cssContainingText('.button', 'Black'));
+  var rainbowBox = element(by.cssContainingText('.button', 'Rainbow'));
 
-  var cheeseBox = element(by.cssContainingText('.box', 'Cheeseburgers'));
-  var fireBox = element(by.cssContainingText('.box', 'Fire'));
-  var poopsBox = element(by.cssContainingText('.box', 'Poops'));
-  var carsBox = element(by.cssContainingText('.box', 'Cars'));
-  var donutsBox = element(by.cssContainingText('.box', 'Donuts'));
-  var phonesBox = element(by.cssContainingText('.box', 'Phones'));
-  var sushiBox = element(by.cssContainingText('.box', 'Sushi'));
-  var baseballsBox = element(by.cssContainingText('.box', 'Baseballs'));
-  var bicyclesBox = element(by.cssContainingText('.box', 'Bicycles'));
+  var cheeseBox = element(by.cssContainingText('.button', 'Cheeseburgers'));
+  var fireBox = element(by.cssContainingText('.button', 'Fire'));
+  var poopsBox = element(by.cssContainingText('.button', 'Poops'));
+  var carsBox = element(by.cssContainingText('.button', 'Cars'));
+  var donutsBox = element(by.cssContainingText('.button', 'Donuts'));
+  var phonesBox = element(by.cssContainingText('.button', 'Phones'));
+  var sushiBox = element(by.cssContainingText('.button', 'Sushi'));
+  var baseballsBox = element(by.cssContainingText('.button', 'Baseballs'));
+  var bicyclesBox = element(by.cssContainingText('.button', 'Bicycles'));
 
   var returnPass = element(by.id('return-pass'));
 
@@ -55,23 +58,29 @@ describe('the very first test', function() {
     siteInput.clear();
     siteInput.sendKeys('efgh.com');
     expect(magicInput.getAttribute('value')).toEqual('abcd');
+    // expect(magicInput.isPresent()).toEqual(true);
     expect(siteInput.getAttribute('value')).toEqual('efgh.com');
+  })
     // console.log(llamaBox.getWebElement());
-    expect(llamaBox.isPresent()).toEqual(true);
+    // expect(llamaBox.isPresent()).toEqual(true);
     // console.log(llamaBox);
-    llamaBox.click().then(function() {
-      snubsBox.click().then(function() {
-        orangeBox.click().then(function() {
-          donutsBox.click().then(function() {
-            // console.log(returnPass);
-            // console.log(returnPass.getText());
-            // expect(returnPass.length).toEqual(16);
+
+    it('can click the buttons', function() {
+      expect(dogBox.isPresent()).toEqual(true);
+      llamaBox.click().then(function() {
+          snubsBox.click().then(function() {
+            orangeBox.click().then(function() {
+              donutsBox.click().then(function() {
+        // console.log(returnPass);
+        // console.log(returnPass.getText());
+        // expect(returnPass.length).toEqual(16);
+              })
+            })
           })
-        })
       })
+
     })
 
 
 
-  })
 })
