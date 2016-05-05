8e0d14989be4a9c8553eddef0865c4167b851433
Ryan Flaherty <rcflaherty@gmail.com>
2016-05-04 17:21:41 -0700

    removed unused template content, added about info

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -108,21 +108,10 @@ angular.module('starter.controllers', [])
     $scope.progressBar = '';
     Store.clear();
   }
+
 })
 
-.controller('ChatsCtrl', function($scope, Chats) {
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope) {
+.controller('SettingsCtrl', function($scope) {
   $scope.settings = {
     enableFriends: true
   };
