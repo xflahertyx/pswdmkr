4210f58bb5708da8089a61f77f27d15107c1ac61
Ryan Flaherty <rcflaherty@gmail.com>
2016-05-05 16:19:35 -0700

    updated unit tests, controller cleanup

--- tests/unit-tests/passwordControllerTests.js
+++ tests/unit-tests/passwordControllerTests.js
@@ -27,52 +27,45 @@ describe('PasswordCtrl', function() {
     expect(typeof $scope.nouns).toBe('function');
     expect(typeof $scope.displayPass).toBe('function');
     expect(typeof $scope.copyPass).toBe('function');
-    expect(typeof $scope.clearAll).toBe('function');
+    expect(typeof $scope.copyPass).toBe('function');
+    expect(typeof $scope.backButton).toBe('function');
   });
   it('should have an object', function() {
     expect(typeof $scope.test).toBe('object');
   });
 
-  it('should assign "dog" values to $scope', function() {
-    $scope.animals('dog');
-    expect($scope.progressBar).toBe('dog ');
-    $scope.clearAll();
-    expect($scope.progressBar).toBe('');
-  });
-  it('should assign "verbs" values to $scope', function() {
-    $scope.animals('cat');
-    $scope.verbs('dreams')
-    expect($scope.progressBar).toBe('cat dreams ');
-    $scope.clearAll();
-    expect($scope.progressBar).toBe('');
-  });
-  it('should assign "colors" values to $scope', function() {
-    $scope.animals('llama');
-    $scope.verbs('smells');
-    $scope.colors('purple');
-    expect($scope.progressBar).toBe('llama smells purple ');
-    $scope.clearAll();
-    expect($scope.progressBar).toBe('');
-  });
-  it('should assign "nouns" values to $scope', function() {
-    $scope.animals('dragon');
-    $scope.verbs('breathes');
-    $scope.colors('orange');
-    $scope.nouns('fire');
-    expect($scope.progressBar).toBe('dragon breathes orange fire');
-    $scope.clearAll();
-    expect($scope.progressBar).toBe('');
-  });
-  it('should take values, and return a password', function() {
+  it('should values to $scope, and return a password', function() {
     $scope.test.siteInput = 'pass';
     $scope.test.magicInput = 'word';
     $scope.animals('dragon');
     $scope.verbs('breathes');
     $scope.colors('orange');
     $scope.nouns('fire');
+    expect($scope.animalChoice).toBe('dragon');
+    expect($scope.verbChoice).toBe('breathes');
+    expect($scope.colorChoice).toBe('orange');
+    expect($scope.nounChoice).toBe('fire');
     expect($scope.displayPass()).toBe('K&LV}!tHA?^Y`0`|');
     $scope.clearAll();
-    expect($scope.progressBar).toBe('');
+    expect($scope.animalChoice).toBe('');
+    expect($scope.verbChoice).toBe('');
+    expect($scope.colorChoice).toBe('');
+    expect($scope.nounChoice).toBe('');
+  });
+  it('should take values and then clear them on backButton', function() {
+    $scope.test.siteInput = 'pass';
+    $scope.test.magicInput = 'word';
+    $scope.animals('dragon');
+    $scope.verbs('breathes');
+    $scope.colors('orange');
+    expect($scope.animalChoice).toBe('dragon');
+    expect($scope.verbChoice).toBe('breathes');
+    expect($scope.colorChoice).toBe('orange');
+    $scope.backButton('colorChoice');
+    expect($scope.animalChoice).toBe('dragon');
+    expect($scope.verbChoice).toBe('breathes');
+    expect($scope.colorChoice).toBe('');
+    $scope.clearAll();
   });
 
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -45,7 +45,6 @@ angular.module('starter.controllers', [])
   $scope.test = {};
   $scope.test.siteInput = '';
   $scope.test.magicInput = '';
-  $scope.progressBar = '';
 
   $scope.animals = function(ani) {
     var inputs = $scope.test.siteInput +''+ $scope.test.magicInput
@@ -55,28 +54,24 @@ angular.module('starter.controllers', [])
     var extended = algorithms.extend(filtered);
     var mix = algorithms.mixup(extended, list[ani]);
     Store.set('animal', algorithms.changer(mix, list[ani]));
-    // $scope.progressBar += ani + ' ';
     $scope.animalChoice = ani;
   };
   $scope.verbs = function(ver) {
     var input = Store.get('animal');
     var change1 = algorithms.changer(input, list[ver][0]);
     Store.set('verb', change1);
-    // $scope.progressBar += ver + ' ';
     $scope.verbChoice = ver;
   };
   $scope.colors = function(col) {
     var input = Store.get('verb');
     var change1 = algorithms.changer(input, list[col][0]);
     Store.set('color', change1);
-    // $scope.progressBar += col + ' ';
     $scope.colorChoice = col
   };
   $scope.nouns = function(noun) {
     var input = Store.get('color');
     var change1 = algorithms.changer(input, list[noun][0]);
     Store.set('noun', change1);
-    // $scope.progressBar += noun;
     $scope.nounChoice = noun;
   }
   $scope.displayPass = function() {
